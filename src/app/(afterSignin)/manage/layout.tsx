4776c57f64a5618639c6eb7b999998b66865deeb
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-03-09 18:12:22 +0900

    fix: 대시보드로 왔을 때 token 체크

--- src/app/(afterSignin)/manage/layout.tsx
+++ src/app/(afterSignin)/manage/layout.tsx
@@ -1,10 +1,29 @@
 'use client';
 
-import { ReactNode } from 'react';
-import Sidebar from './_components/sidebar/sidebar';
 import styles from './layout.module.scss';
 
+import { ReactNode, useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { getTokenInCookies, setTokenInCookies } from '@utils';
+import Sidebar from './_components/sidebar/sidebar';
+
 export default function Layout({ children }: { children: ReactNode }) {
+  const params = useSearchParams().get('token');
+  const [token, setToken] = useState<string>('');
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      if (params) {
+        setTokenInCookies(params);
+      }
+
+      const token = await getTokenInCookies();
+      setToken(token || '');
+    };
+
+    fetchToken();
+  }, [params]);
+
   return (
     <div className={styles.layout}>
       <div className={styles.sidebar}>
