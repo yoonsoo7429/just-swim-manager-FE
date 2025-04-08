412bc32937baba3035848d709fff9f2cc8227cf6
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2025-04-08 12:13:34 +0900

    fix: 로그인 후에 다음 동작에 대한 에러 수정

--- src/app/(afterSignin)/manage/layout.tsx
+++ src/app/(afterSignin)/manage/layout.tsx
@@ -3,24 +3,38 @@
 import styles from './layout.module.scss';
 
 import { ReactNode, useEffect, useState } from 'react';
-import { useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { getTokenInCookies, setTokenInCookies } from '@utils';
 import Sidebar from './_components/sidebar/sidebar';
+import { ROUTES } from '@data';
 
 export default function Layout({ children }: { children: ReactNode }) {
   const params = useSearchParams().get('token');
+  const router = useRouter();
+  const [ready, setReady] = useState(false);
 
   useEffect(() => {
     const fetchToken = async () => {
       if (params) {
-        setTokenInCookies(params);
+        await setTokenInCookies(params);
+        setReady(true);
+        const cleanUrl = window.location.pathname;
+        router.replace(cleanUrl);
+        return;
       }
 
-      await getTokenInCookies();
+      const cookieToken = await getTokenInCookies();
+
+      if (!cookieToken) {
+        router.replace(ROUTES.ONBOARDING.signin);
+        return;
+      }
     };
 
     fetchToken();
   }, [params]);
+
+  if (!ready) return null;
 
   return (
     <div className={styles.layout}>
